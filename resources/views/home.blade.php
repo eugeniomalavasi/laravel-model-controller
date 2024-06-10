171f3cad564cbff8245dfd495da96a189b6d1c30
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-06-10 14:52:03 +0200

    feat: bootstrap card used to present data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Lista Film</h1>
-    <ul>
-        @foreach ($movieList as $movie)
-            <li>{{$movie->title}}</li>
-            <li>{{$movie->vote}}</li>
-            </ul>
-            <img src="{{$movie->image}}" alt="">
-        @endforeach
+<div class="container">
+        <h1>Lista Film</h1>
+        <div class="row">
+            @foreach ($movieList as $movie)
+                <div class="col-sm-12 col-md-6 col-lg-3">
+                    @include('partials.cards')
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>Film</title>
 
   <!-- Includiamo gli assets con la direttiva vite -->
   @vite('resources/js/app.js')
@@ -13,9 +13,7 @@
 
 <body>
   <main>
-    <div class="container">
       @yield('content')
-    </div>
   </main>
 
 </body>
